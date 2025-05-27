5253731019e98cdaa29980e73e2f0b05e4d0cff2
Silvina <msilvinahippler@gmail.com>
2025-05-27 00:31:41 -0300

    dibujos.cpp

---
+++ dibujos.cpp
@@ -0,0 +1,27 @@
+
+//para poner el nombre del dr
+
+
+                ooo                
+                ooo                
+                o o                
+               o   o               
+              o     o              
+             o       o             
+            o         o            
+           o           o           
+ MMMMMMMMMNMMMMMMMMMMMMMNMMMMMNMMM 
+M                                 M
+M                                 M
+M                                 M
+M                                 M
+M                                 M
+M                                 M
+M                                 M
+M                                 M
+M                                 M
+M                                 M
+ MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM 
+
+
+                                   
